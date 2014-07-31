b73d57d6c4a63d933854eabadf68249606802e79
Linxiangyu <lxyweb@gmail.com>
2014-07-31 14:27:36 +0800

    Initial Commit

---
+++ FLEX/FLEX.h
@@ -0,0 +1,13 @@
+//
+//  FLEX.h
+//  FLEX
+//
+//  Created by xiangyu on 7/31/14.
+//  Copyright (c) 2014 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FLEX : NSObject
+
+@end
---
+++ FLEX/FLEX.m
@@ -0,0 +1,22 @@
+//
+//  FLEX.m
+//  FLEX
+//
+//  Created by xiangyu on 7/31/14.
+//  Copyright (c) 2014 __MyCompanyName__. All rights reserved.
+//
+
+#import "FLEX.h"
+
+@implementation FLEX
+
+-(id)init
+{
+	if ((self = [super init]))
+	{
+	}
+    
+	return self;
+}
+
+@end
